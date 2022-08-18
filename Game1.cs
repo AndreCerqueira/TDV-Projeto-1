cd88db633f59b2b8bf4cb51b04f76eab65b485e5
unknown <jamesdoth@gmail.com>
2022-08-18 09:52:57 -0700

    loaded assets. player class init

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,17 @@ namespace rpg
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D playerSprite;
+        Texture2D walkDown;
+        Texture2D walkUp;
+        Texture2D walkLeft;
+        Texture2D walkRight;
+
+        Texture2D background;
+        Texture2D ball;
+        Texture2D skull;
+
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -18,7 +29,9 @@ namespace rpg
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -27,7 +40,15 @@ namespace rpg
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            playerSprite = Content.Load<Texture2D>("Player/player");
+            walkDown = Content.Load<Texture2D>("Player/walkDown");
+            walkUp = Content.Load<Texture2D>("Player/walkUp");
+            walkLeft = Content.Load<Texture2D>("Player/walkLeft");
+            walkRight = Content.Load<Texture2D>("Player/walkRight");
+
+            background = Content.Load<Texture2D>("background");
+            ball = Content.Load<Texture2D>("ball");
+            skull = Content.Load<Texture2D>("skull");
         }
 
         protected override void Update(GameTime gameTime)
@@ -44,7 +65,9 @@ namespace rpg
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(background, new Vector2(-500, -500), Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace rpg
+{
+    class Player
+    {
+        private Vector2 position = new Vector2(500, 300);
+        private int speed = 300;
+
+        public Vector2 Position
+        {
+            get
+            {
+                return position;
+            }
+        }
+        public void setX(float newX)
+        {
+            position.X = newX;
+        }
+        public void setY(float newY)
+        {
+            position.Y = newY;
+        }
+    }
+}
